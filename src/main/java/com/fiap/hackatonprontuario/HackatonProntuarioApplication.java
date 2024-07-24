0843b6b5fce57f96b286c784b6b9a4db3c3e5427
Samuel Almeida <barbosasamuel24@gmail.com>
2024-07-23 21:46:21 -0300

    first commit

---
+++ src/main/java/com/fiap/hackatonprontuario/HackatonProntuarioApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.hackatonprontuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonProntuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonProntuarioApplication.class, args);
+	}
+
+}
